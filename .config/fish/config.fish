d05f5016073bb4f4c5cb2daf0c9976f8532a1dba
Nurlyx <nurlyx@proton.me>
2025-08-09 15:58:09 +0530

    fix: idk

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,6 @@ set -U fish_greeting
 starship init fish | source
 
 alias toggle "astal-mpris play-pause"
-alias neovim = nvim
-alias vim = nvim
-alias v = nvim
+alias neovim nvim
+alias vim nvim
+alias v nvim
