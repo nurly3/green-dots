067e2cbb34dbf563406f5e874f6a43e963c0db98
Nurlyx <nurlyx@github.com>
2025-08-18 06:22:04 +0200

    neovim changes

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,7 +1,3 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
 local o = vim.opt
 vim.g.snacks_animate = false
 o.wrap = true
